e22d428ab92804b64e576b68ff5e535083df2480
ramaalmahmoud <ramaoudat151@gmail.com>
2024-07-01 22:19:54 +0300

    task5

---
+++ 1-7-2024/1-7-2024/Car.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_7_2024
+{
+    internal class Car
+    {
+        protected string Make { set; get; }
+        protected int Year { set; get; }
+        protected string Type { set; get; }
+        protected int Price { set; get; }
+        protected string Model { set; get; }
+        protected int Number { set; get; }
+        protected string Color { set; get; }
+        protected bool Engine { set; get; }
+
+        public Car()
+        {
+            Make = "Toyota";
+            Year = 2022;
+            Type = "Sedan";
+            Price = 25000;
+            Model = "Camry";
+            Number = 1234;
+            Color = "Red";
+            Engine = true;
+
+        }
+
+        public Car(string make, int year, string type, int price, string model, int number, string color, bool engine)
+        {
+            Make = make;
+            Year = year;
+            Type = type;
+            Price = price;
+            Model = model;
+            Number = number;
+            Color = color;
+            Engine = engine;
+
+        }
+        public void startAndStop()
+        {
+            if (Engine)
+            {
+                Console.WriteLine("The engine start");
+
+            }
+            else
+            {
+                Console.WriteLine("The engine stop");
+                Engine = false;
+            }
+
+
+
+        }
+
+        public string fullInfo()
+        {
+            return $"Make: {Make}, Year: {Year}, Type: {Type}, Price: {Price}, Model: {Model}, Number: {Number}, Color: {Color}";
+
+        }
+    }
+}
---
+++ 1-7-2024/1-7-2024/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_7_2024
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Car's make");
+            string Make = Console.ReadLine();
+            Console.WriteLine("Enter Car's year");
+            int Year = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Car's type");
+            string Type = Console.ReadLine();
+            Console.WriteLine("Enter Car's price");
+            int Price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Car's model");
+            string Model = Console.ReadLine();
+            Console.WriteLine("Enter Car's number"); ;
+            int Number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Car's color");
+            string Color = Console.ReadLine();
+            Console.WriteLine("Start the car");
+            bool Engine = Convert.ToBoolean(Console.ReadLine());
+            Car car = new Car(Make, Year, Type, Price, Model, Number, Color, Engine);
+            Console.WriteLine(car.fullInfo());
+            car.startAndStop();
+
+            BMW bmw = new BMW();
+            Console.WriteLine(bmw.fullInfo());
+
+            bmw.startAndStop();
+
+        }
+    }
+}
