b7bea2bdaabf934af7617ae7fded87c536e5fe28
ramaalmahmoud <ramaoudat151@gmail.com>
2024-07-02 22:02:03 +0300

    update task6

---
+++ 2-7-2024/2-7-2024/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_7_2024
+{
+    internal class Program
+    { public interface ISound
+        {
+            void MakeSound();
+        }
+
+        public abstract class Animal:ISound
+        {
+            public string Name { get; set; }
+            protected Animal(string name)
+            {
+                Name = name;
+            }
+
+            public abstract void Eat();
+           public void Sleep()
+            {
+                Console.WriteLine($"{Name} is sleeping");
+            }
+
+            public void MakeSound()
+            {
+                Console.WriteLine("The animal make sound");
+            }
+        }
+
+        class Dog : Animal
+        {
+            public Dog(string name) : base(name)
+            {
+            }
+
+            public override void Eat()
+            {
+                Console.WriteLine($" {Name} eat meat");
+            }
+            public void MakeSound()
+            {
+                Console.WriteLine($" {Name} make Woof!");
+            }
+        }
+
+        class Cat : Animal
+        {
+            public Cat(string name) : base(name)
+            {
+            }
+
+            public override void Eat()
+            {
+                Console.WriteLine($" {Name} eat fish");
+            }
+            public void MakeSound()
+            {
+                Console.WriteLine($" {Name} make Meow!");
+            }
+        }
+        static void Main(string[] args)
+        {
+
+            Dog dog = new Dog("Seemaa");
+            dog.MakeSound();
+            dog.Eat();
+            dog.Sleep();
+
+            Cat cat = new Cat("Aless");
+            cat.MakeSound();
+            cat.Eat();
+            cat.Sleep();
+
+
+        }
+    }
+}
